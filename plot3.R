c2204cf3f50928db1c67fdf686a3ec5e64b11698
Marion Grould <grould.marion@gmail.com>
2017-11-14 15:38:57 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -23,13 +23,9 @@ plot(DateTime, subdata$Sub_metering_1,
      xlab = "",
      ylab = "Energy sub metering",
      type = "l")
-lines(DateTime, subdata$Sub_metering_2,
-      type = "l",
-      col = "red")
-lines(DateTime, subdata$Sub_metering_3,
-      type = "l",
-      col = "blue")
-legend("topright", lty = "solid", col=c("black", "red", "blue"), 
-       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+lines(DateTime, subdata$Sub_metering_2, type = "l", col = "red")
+lines(DateTime, subdata$Sub_metering_3, type = "l", col = "blue")
+legend("topright", lty = "solid", col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.off()
 
